a192ea173e6267c02fe4cd1b2416957f47fb6921
Guedson Quispe Anchaise <romarioanchaise@gmail.com>
2026-05-19 19:56:00 -0500

    Interfaz mejorada

--- server.js
+++ server.js
@@ -1,30 +1,9 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+function mostrarMensaje() {
+    const mensaje = document.getElementById('mensaje');
+    mensaje.classList.remove('oculto');
+    setTimeout(() => {
+        mensaje.classList.add('oculto');
+    }, 3000);
+}
 
-const server = http.createServer((req, res) => {
-    let filePath = '.' + req.url;
-    if (filePath == './') filePath = './index.html';
-
-    const extname = path.extname(filePath).toLowerCase();
-    const mimeTypes = {
-        '.html': 'text/html',
-        '.js': 'application/javascript',
-        '.css': 'text/css'
-    };
-    const contentType = mimeTypes[extname] || 'application/octet-stream';
-
-    fs.readFile(filePath, (error, content) => {
-        if (error) {
-            res.writeHead(404);
-            res.end('Error 404: No encontrado');
-        } else {
-            res.writeHead(200, { 'Content-Type': contentType });
-            res.end(content, 'utf-8');
-        }
-    });
-});
-
-server.listen(4000, () => {
-    console.log('Servidor corriendo en http://localhost:4000');
-});
+console.log('MiApp cargada correctamente!');
